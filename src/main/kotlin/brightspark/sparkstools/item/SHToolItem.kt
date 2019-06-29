ac59ab2a8c2a0e478dd7cee08d2846db9b3dd41b
bright_spark <markpassey@btinternet.com>
2019-06-29 03:46:14 +0100

    Fixed tools not taking damage and other material related properties

--- src/main/kotlin/brightspark/sparkstools/item/SHToolItem.kt
+++ src/main/kotlin/brightspark/sparkstools/item/SHToolItem.kt
@@ -2,18 +2,29 @@ package brightspark.sparkstools.item
 
 import brightspark.sparkstools.SparksTools
 import brightspark.sparkstools.ToolUtils
+import com.google.common.collect.Multimap
 import net.minecraft.block.state.IBlockState
+import net.minecraft.entity.EntityLivingBase
+import net.minecraft.entity.SharedMonsterAttributes
+import net.minecraft.entity.ai.attributes.AttributeModifier
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.inventory.EntityEquipmentSlot
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
+import net.minecraftforge.oredict.OreDictionary
 
 abstract class SHToolItem(val tool: CustomTool) : Item() {
     init {
 	    registryName = tool.registryName
         translationKey = tool.registryName.path
         creativeTab = SparksTools.tab
+	    maxStackSize = 1
+	    maxDamage = tool.durability
     }
 
 	internal abstract fun getBlocksToBreakIfEffective(stack: ItemStack, pos: BlockPos, side: EnumFacing, player: EntityPlayer): Iterable<BlockPos>
@@ -46,7 +57,35 @@ abstract class SHToolItem(val tool: CustomTool) : Item() {
             getHarvestLevel(stack, requiredTool, null, state) >= state.block.getHarvestLevel(state)
     }
 
-    override fun getDestroySpeed(stack: ItemStack, state: IBlockState): Float =
+	override fun hitEntity(stack: ItemStack, target: EntityLivingBase, attacker: EntityLivingBase): Boolean {
+		stack.damageItem(2, attacker)
+		return true
+	}
+
+	override fun onBlockDestroyed(stack: ItemStack, worldIn: World, state: IBlockState, pos: BlockPos, entityLiving: EntityLivingBase): Boolean {
+		if (!worldIn.isRemote && state.getBlockHardness(worldIn, pos) > 0F)
+			stack.damageItem(1, entityLiving)
+		return true
+	}
+
+	@SideOnly(Side.CLIENT)
+	override fun isFull3D(): Boolean = true
+
+	override fun getItemEnchantability(stack: ItemStack): Int = tool.enchantability
+
+	override fun getIsRepairable(toRepair: ItemStack, repair: ItemStack): Boolean =
+		tool.material.any { OreDictionary.itemMatches(repair, it, false) }
+
+	override fun getAttributeModifiers(slot: EntityEquipmentSlot, stack: ItemStack): Multimap<String, AttributeModifier> {
+		val attributes = super.getAttributeModifiers(slot, stack)
+		if (slot == EntityEquipmentSlot.MAINHAND) {
+			attributes.put(SharedMonsterAttributes.ATTACK_DAMAGE.name, AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", tool.attackDamage.toDouble(), 0))
+			attributes.put(SharedMonsterAttributes.ATTACK_SPEED.name, AttributeModifier(ATTACK_SPEED_MODIFIER, "Tool modifier", tool.attackSpeed.toDouble(), 0))
+		}
+		return attributes
+	}
+
+	override fun getDestroySpeed(stack: ItemStack, state: IBlockState): Float =
         if (isEffective(stack, state)) tool.efficiency else 1F
 
     override fun getHarvestLevel(stack: ItemStack, toolClass: String, player: EntityPlayer?, blockState: IBlockState?): Int =
