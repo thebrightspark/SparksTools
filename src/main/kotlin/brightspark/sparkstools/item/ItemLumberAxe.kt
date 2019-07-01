6ced6f87f6e4c20bd17d0579d8f43b6de965d4ac
bright_spark <markpassey@btinternet.com>
2019-07-01 11:20:40 +0100

    Lumber axe now only breaks connected blocks of logs when not sneaking

--- src/main/kotlin/brightspark/sparkstools/item/ItemLumberAxe.kt
+++ src/main/kotlin/brightspark/sparkstools/item/ItemLumberAxe.kt
@@ -2,12 +2,18 @@ package brightspark.sparkstools.item
 
 import brightspark.sparkstools.SHConfig
 import brightspark.sparkstools.ToolUtils
+import net.minecraft.block.BlockLog
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.ItemStack
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.BlockPos
 
 class ItemLumberAxe(tool: CustomTool) : SHToolItem(tool) {
-	override fun getBlocksToBreakIfEffective(stack: ItemStack, pos: BlockPos, side: EnumFacing, player: EntityPlayer): Iterable<BlockPos> =
-		ToolUtils.getConnectedBlocks(pos, player.world, SHConfig.lumberAxeMaxBlocks)
+	override fun getBlocksToBreakIfEffective(stack: ItemStack, pos: BlockPos, side: EnumFacing, player: EntityPlayer): Iterable<BlockPos> {
+		val world = player.world
+		return if (!player.isSneaking && world.getBlockState(pos).block is BlockLog)
+			ToolUtils.getConnectedBlocks(pos, player.world, SHConfig.lumberAxeMaxBlocks)
+		else
+			ToolUtils.getSquareBreakArea(stack, pos, side, player)
+	}
 }
